d8ecbd665be00ec6fe338f8e986524400cf9d35d
gabrielrportugal <gabrielrportugal@outlook.com>
2021-03-15 18:39:48 -0300

    Finalizando desafio 2 do ignite react

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,42 @@
-export function Content() {
-  // Complete aqui
+import {useState, useEffect} from 'react';
+import { MovieCard } from './MovieCard';
+
+import {MovieProps, GenreResponseProps} from '../interfaces/index';
+
+import { api } from '../services/api';
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  selectedGenreId: number;
+  setSelectedGenre: React.Dispatch<React.SetStateAction<GenreResponseProps>>;
+}
+
+export function Content({selectedGenre, selectedGenreId, setSelectedGenre}:ContentProps) {
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
---
+++ src/interfaces/index.ts
@@ -0,0 +1,21 @@
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
